1e55627f944c6467c9d12c2a319e05b82294cb05
Zack Jones <zajonesck83@gmail.com>
2022-10-15 19:32:39 -0500

    npm

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Clients from "./components/Clients.vue";
+// import Weights from "./components/weights.vue";
 </script>
 
 <template>
-  <Clients />
+  <!-- <div id="nav">
+    <router-link to="/">Clients</router-link> |
+    <router-link to="/weights">Weights</router-link>
+  </div>
+  <router-view /> -->
 </template>
 
 <style scoped></style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -3,4 +3,30 @@ import App from "./App.vue";
 
 import "./assets/main.css";
 
+// import { createRouter, createWebHistory } from "vue-router";
+// import Weights from "./components/Weights.vue";
+
+// const routes = [
+//   {
+//     path: "/",
+//     name: "Clients",
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ "./components/Clients.vue"),
+//   },
+//   {
+//     path: "/Weights",
+//     name: "Weights",
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ "./components/Weights.vue"),
+//   },
+// ];
+// const router = createRouter({
+//   history: createWebHistory(process.env.prod),
+//   routes,
+// });
+// export default router;
+
 createApp(App).mount("#app");
