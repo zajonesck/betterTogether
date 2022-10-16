126884d8dda26cd1d12c23e0679efe9be4c256bd
Zack Jones <zajonesck83@gmail.com>
2022-10-15 21:50:07 -0500

    fixed router vue history mode

--- ui/dist/assets/Weights.bcddbaa3.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.0840909a.js";const e={};function r(c,o){return"Go to the gym!"}const n=t(e,[["render",r]]);export{n as default};
---
+++ ui/dist/assets/Workouts.b3e57074.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.e9bb5395.js";const s={};function t(c,e){return"Push ups"}const _=r(s,[["render",t]]);export{_ as default};
