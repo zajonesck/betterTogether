4f151734f69380b9fdde9f9f0c7ef4addc85a0f4
Zack Jones <zajonesck83@gmail.com>
2022-10-16 19:23:11 -0500

    vue router hash mode

--- ui/dist/assets/Workouts.17f0268b.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.b1e1ce99.js";const s={};function t(c,e){return"Push ups"}const _=r(s,[["render",t]]);export{_ as default};
---
+++ ui/dist/assets/Workouts.e1748e02.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.4deafada.js";const s={};function t(c,e){return"Push ups"}const _=r(s,[["render",t]]);export{_ as default};
