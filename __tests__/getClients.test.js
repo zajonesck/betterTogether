00050ba166a1b6250e383bbe76cec987bfcecd42
Zack Jones <zajonesck83@gmail.com>
2022-11-30 20:17:45 -0600

    post clients name error handling test

---
+++ __tests__/getClients.test.js
@@ -0,0 +1,6 @@
+// const request = require("supertest");
+// const baseURL = "http://localhost:3000";
+// const app = require("../app");
+
+//GET
+//test const getClients
---
+++ __tests__/postClients.test.js
@@ -0,0 +1,39 @@
+const request = require("supertest");
+const baseURL = "http://localhost:3000";
+const app = require("../app");
+
+describe("POST /clients", () => {
+  test("It responds with the newly created client", async () => {
+    const response = await request(baseURL).get("/clients");
+    const numberOfClients = response.body.length;
+    const newClient = await request(baseURL).post("/clients").send({
+      client_name: "New client",
+      birth_day: "10/10/1010",
+    });
+    //todo: fix test
+    const newResponse = await request(baseURL).get("/clients");
+    expect(newResponse.body.length).toBe(numberOfClients + 1);
+    expect(newClient.body).toStrictEqual([]);
+    expect(newClient.statusCode).toBe(200);
+  });
+  test("Throws 400 error when there is no client_name", async () => {
+    const newClient = await request(baseURL).post("/clients").send({
+      client_name: "",
+      birth_day: "10/10/1010",
+    });
+    expect(newClient.body).toStrictEqual({});
+    console.log(newClient, "newclient.text");
+    expect(newClient.text).toEqual("Client name required.");
+    expect(newClient.statusCode).toBe(400);
+  });
+});
+
+describe("GET /clients", () => {
+  test("It responds with an array of clients", async () => {
+    const response = await request(baseURL).get("/clients");
+    console.log("response: ", response);
+    expect(response.body[0]).toHaveProperty("id");
+    expect(response.body[0]).toHaveProperty("client_name");
+    expect(response.statusCode).toBe(200);
+  });
+});
