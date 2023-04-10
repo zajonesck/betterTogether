617f2a09f8f63caa2c19271de94e011e1090f166
Zack Jones <zajonesck83@gmail.com>
2023-04-09 22:48:24 -0500

    take 4 line graph

--- ui/src/views/WeightLineGraph.vue
+++ ui/src/views/WeightLineGraph.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <line-chart :data="chartData" :options="chartOptions" />
+    <line-chart :chart-data="chartData" />
   </div>
 </template>
 
@@ -8,35 +8,14 @@
 import { Line } from "vue3-chart-v2";
 
 export default {
-  components: {
-    LineChart: Line,
-  },
   props: {
     chartData: {
       type: Object,
       required: true,
     },
   },
-  data() {
-    return {
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [
-            {
-              type: "time",
-              time: {
-                unit: "day",
-                displayFormats: {
-                  day: "MMM DD",
-                },
-              },
-            },
-          ],
-        },
-      },
-    };
+  components: {
+    LineChart: Line,
   },
 };
 </script>
