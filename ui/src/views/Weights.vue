30eec6bc985c7968ad9343c1af4d112f2cdfc383
Zack Jones <zajonesck83@gmail.com>
2023-04-09 22:21:52 -0500

    remove the 's' character in the data() function: in weights .vue

--- ui/src/views/Weights.vue
+++ ui/src/views/Weights.vue
@@ -8,7 +8,7 @@ import LineGraph from "@/views/LineGraph.vue";
 export default {
   data() {
     return {s
-      clientWeights: [],
+      clientWeight: [],
       newWeight: "",
       newWeighDate: "",
       clientName: "",
