e0e11a13f956dc5fdcf2b4cecf6485ce52ce7b3f
Zack Jones <zajonesck83@gmail.com>
2023-07-20 20:05:19 -0500

    server.js update

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("listening on port 3000!");
+// Use process.env.PORT if it's available (in Heroku's environment), otherwise fall back to 3000
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
 });
