42d909485be41ff00386a3f2f21bcb7ee5701fa8
Zack Jones <zajonesck83@gmail.com>
2023-07-28 19:22:23 -0500

    hamburger menu

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <v-app>
     <v-app-bar app dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer">
+        <v-icon color="black">mdi-menu</v-icon>
+      </v-app-bar-nav-icon>
       <v-app-bar-title theme="dark">Better Together</v-app-bar-title>
       <v-spacer></v-spacer>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-btn text to="/client-roster" color="black">Client List</v-btn>
-      <v-btn text to="/workout-rx" color="black">Workouts</v-btn>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item link to="/client-roster">
+        <v-list-item-icon>
+          <v-icon>mdi-account-group</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Client List</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link to="/workout-rx">
+        <v-list-item-icon>
+          <v-icon>mdi-dumbbell</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Workouts</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
 
     <v-main>
       <v-container fluid>
         <router-view />
       </v-container>
     </v-main>
+
     <v-footer padless dark>
       <v-container fluid class="white--text">
         <v-row justify="space-between" align="center">
@@ -46,7 +65,10 @@
   </v-app>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const drawer = ref(false);
+</script>
 
 <style scoped>
 #app {
