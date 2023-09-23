be8cad9fab5a79f61f2584209ce4be4ebcd26221
Chelsea Green <greenchelc@gmail.com>
2023-09-22 21:03:57 -0500

    fix broken ui build

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,24 +1,14 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    "process.env": process.env,
-    global: "window",
-  },
   resolve: {
     alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-      "@": "/src", // This is a common alias to refer to the "src" directory.
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./src/main.js", // Your main entry file
-      },
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
