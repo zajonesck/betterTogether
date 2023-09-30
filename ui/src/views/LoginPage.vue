8f9c4b77042c874f0b80faea2baeaab2c0a95ca6
Zack Jones <zajonesck83@gmail.com>
2023-09-30 13:13:01 -0500

    modified login and signup buttons

--- ui/src/views/LoginPage.vue
+++ ui/src/views/LoginPage.vue
@@ -1,39 +1,41 @@
 <template>
-  <v-container class="flex-container" style="min-height: calc(100vh - 250px)">
-    <v-row justify="center" align="center" class="fill-height">
-      <v-col cols="8" sm="8" md="8">
-        <v-card>
-          <v-card-title>Login</v-card-title>
-          <v-card-text>
-            <v-alert v-if="error" type="error">{{ error }}</v-alert>
-            <v-form ref="form">
-              <v-text-field
-                v-model="email"
-                label="Email"
-                required
-                autocomplete="email"
-              ></v-text-field>
+  <div>
+    <v-container class="flex-container" style="min-height: calc(100vh - 250px)">
+      <v-row justify="center" align="center" class="fill-height">
+        <v-col cols="8" sm="8" md="8">
+          <v-card>
+            <v-card-title>Login</v-card-title>
+            <v-card-text>
+              <v-alert v-if="error" type="error">{{ error }}</v-alert>
+              <v-form ref="form">
+                <v-text-field
+                  v-model="email"
+                  label="Email"
+                  required
+                  autocomplete="email"
+                ></v-text-field>
 
-              <v-text-field
-                v-model="password"
-                label="Password"
-                type="password"
-                required
-                autocomplete="current-password"
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions class="justify-center">
-            <v-btn @click="login" class="mx-2">Login</v-btn>
-          </v-card-actions>
-        </v-card>
-        <v-row justify="center" class="mt-4">
-          <v-btn text @click="tryAsGuest" class="mx-2">Try as Guest</v-btn>
-          <v-btn text @click="signUp" class="mx-2">Sign Up</v-btn>
-        </v-row>
-      </v-col>
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  type="password"
+                  required
+                  autocomplete="current-password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn @click="login">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-row justify="center" class="mt-1">
+      <v-btn text @click="tryAsGuest" class="mx-2">Try as Guest</v-btn>
+      <v-btn text @click="signUp" class="mx-2">Sign Up</v-btn>
     </v-row>
-  </v-container>
+  </div>
 </template>
 
 <script>
@@ -81,5 +83,3 @@ export default {
   },
 };
 </script>
-
-<style></style>
